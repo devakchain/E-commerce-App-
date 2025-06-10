54b6481b7d384a164f9cbba6e6c034c48e7280f6
devakchain <shmoelyitzhak@gmail.com>
2025-06-10 22:06:17 +0300

    build

--- e-commerce/frontend/src/components/ProductCardSkelton.jsx
+++ e-commerce/frontend/src/components/ProductCardSkelton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import "react-loading-skeleton/dist/skeleton.css"; // ✅ נכון
 
 function ProductCardSkelton() {
   return <Skeleton className="product_card" width={"275px"} />;
