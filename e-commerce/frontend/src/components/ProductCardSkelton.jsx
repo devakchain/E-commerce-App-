c72710ce7316a785f604d78112e0263af1a4ad36
devakchain <shmoelyitzhak@gmail.com>
2025-06-10 21:59:53 +0300

    import libary

--- e-commerce/frontend/src/components/ProductCardSkelton.jsx
+++ e-commerce/frontend/src/components/ProductCardSkelton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/Skeleton.css";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function ProductCardSkelton() {
   return <Skeleton className="product_card" width={"275px"} />;
